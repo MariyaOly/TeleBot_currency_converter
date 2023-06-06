a3673827b1d8417c5f7f8e3ce5e507589cd5fced
Mariya Olyushkina <125290632+MariyaOly@users.noreply.github.com>
2023-06-06 16:05:19 +0200

    config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 TOKEN = "6189592081:AAHOzMXniOBZdqkTmt0laVDapPMYDHrR34w"
 
 keys = {
-    "Euro": "EUR",
-    "US Dollar": "USD",
-    "Russian Ruble": "RUB",
-    "Indian Rupee": "INR",
+     "EUR",
+     "USD",
+     "RUB",
+     "INR",
 }
